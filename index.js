37c868b4d89c69e47bced23184dd982b22e9e313
Al Bilal <bil.bank@yahoo.com>
2022-01-11 11:14:34 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+const saturdayFun = (activity = `roller-skate`) => `This Saturday, I want to ${activity}!`
+
+const mondayWork = (todo = `go to the office`) => `This Monday, I will ${todo}.`
+
+function wrapAdjective (feeling= `*`) {
+   return function (adjective){
+       return `You are ${feeling}${adjective}${feeling}!`
+   }
+}
